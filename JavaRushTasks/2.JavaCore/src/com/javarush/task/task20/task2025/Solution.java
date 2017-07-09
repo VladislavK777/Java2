97625dc5b121d165dadddb590372807c0c5ecfbc
vladislavK777 <vlad-klochkov@mail.ru>
2017-07-09 21:20:38 +0300

    First Version

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,118 +1,110 @@
 package com.javarush.task.task20.task2025;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.TreeSet;
 
 /*
 Алгоритмы-числа
 */
 public class Solution {
-    public static void main(String[] args) {
-        long a = System.currentTimeMillis();
-        long[] y = getNumbers(Integer.MAX_VALUE);
-        long b = System.currentTimeMillis();
-        System.out.println("Time: " + (b - a) / 1000 + " seconds " + ((b - a) % 1000) + " ms");
-        System.out.println("Memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024 + " mb");
-        System.out.println(y.length);
-        for(long i : y) {
-            System.out.print(i + ", ");
+    public static long[] getNumbers(long N) {
+        long[] result = null;
+        TreeSet<Long> numbers = new TreeSet<>();
+
+        //Массив степеней
+        final int SIZE = 12;
+        long[][] powLst = new long[SIZE][SIZE];
+
+        //Здесь лежит нарезка цифр
+        int[] lst = new int[20];
+
+
+        label:
+        for (long i = 1; i < N; i++) {
+            // 1.Отбраковываем числа у которых сумма цифр одинаковая (сокращаем до 200 000 тыс варинтов и менее)
+            // 2.Получаем нарезку цифр в виде массива
+            long x = i;
+            int lenCount = 0;
+            int current;
+            int last = Integer.MAX_VALUE;
+            while (x != 0) {
+                current = (int) (x % 10);
+                if ((current != 0 && last != 0) && last < current)
+                    continue label;
+                last = current;
+                lst[lenCount] = current;
+                x = x / 10;
+                lenCount++;
+            }
+
+            //3. Считаем степень, и заполняем таблицу степеней
+            //Находим сумму степеней цифр числа, например сюда поступило 037 с пункта 2
+            long summa1 = 0;
+            for (int j = 0; j < lenCount; j++) {
+                if (powLst[lst[j]][lenCount] == 0) { //Если в массиве степеней еще нет значения
+                    long a1 = lst[j];
+                    if (a1 != 0 && a1 != 1) {          //Если цифра 0 или 1, то смысла считать степень нет
+                        long a2 = lst[j];
+                        for (int jj = 1; jj < lenCount; jj++) //Считаем степень
+                            a1 *= a2;
+                    }
+                    powLst[lst[j]][lenCount] = a1;//Добавляем в массив степеней новое значение
+                }
+                summa1 += powLst[lst[j]][lenCount];
+            }
+            //Например, Сумма степеней получилось = 370
+
+            //4. Получаем нарезку цифр
+            long xx = summa1;
+            lenCount = 0;
+            while (xx != 0) {
+                lst[lenCount] = (int) (xx % 10);
+                lenCount++;
+                xx = xx / 10;
+            }
+
+            //5. Из полученной суммы берем сумму степеней, для проверки на  число амстронга
+            long summa2 = 0;
+            for (int j = 0; j < lenCount; j++) {
+                if (powLst[lst[j]][lenCount] == 0) { //Если в массиве степеней еще нет значения
+                    long a1 = lst[j];
+                    if (a1 != 0 && a1 != 1) {          //Если цифра 0 или 1, то смысла считать степень нет
+                        long a2 = lst[j];
+                        for (int jj = 1; jj < lenCount; jj++) //Считаем степень
+                            a1 *= a2;
+                    }
+                    powLst[lst[j]][lenCount] = a1;//Добавляем в массив степеней новое значение
+                }
+                summa2 += powLst[lst[j]][lenCount];
+            }
+
+            //6. Добавляем результат
+            if (summa1 == summa2 && summa1 < N)
+                numbers.add(summa1);
         }
+        result = new long[numbers.size()];
+
+        int count = 0;
+        for (Long l : numbers) {
+            result[count] = l;
+            count++;
+        }
+        return result;
     }
 
-    public static long[] getNumbers(long N) {
-        // Init power array
-        final int NUMBERS = 9;
-        final int SQRT = 10;
-        int[][] power = new int[NUMBERS + 1][SQRT + 1];
-        for(int i = 0; i <= NUMBERS; i++) {
-            int temp = i;
-            power[i][1] = temp;
-            for(int j = 2; j <= SQRT; j++) {
-                temp *= i;
-                power[i][j] = temp;
-            }
+    public static void main(String[] args) {
+        Long t0 = System.currentTimeMillis();
+        //int n = 99999999   //1.204
+        //int n = 146511208;
+        //int n = 146511209; //1.978
+        int n = 999999999; //9.62
+        //int n = 2147483647;//22.338 - 24.887
+        long[] lst = getNumbers(n);
+        Long t1 = System.currentTimeMillis();
+        System.out.println("time: " + (t1 - t0) / 1000d + " sec");
+        System.out.println("memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024) + " mb");
+        for (int i = 0; i < lst.length; i++) {
+            System.out.print(lst[i] + ", ");
         }
-        List<Integer> armstrong = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            if(i < 0) break;
-            // All 1-digit numbers are armstrong's numbers
-            if(i > 9) {
-                int n = i;
-                int last;
-                int prev;
-                int degree = 1;
-                boolean shouldVerify = true;
-
-                // Verifying that every digit is less or equals next one
-                // And calculate digits number
-                while(n > 9) {
-                    degree++;
-                    last = n % 10;
-                    n /= 10;
-                    prev = n % 10;
-
-                    if(last == 0) {
-                        last = 10;
-                    }
-
-                    if(prev == 0) {
-                        prev = 10;
-                    }
-
-                    if(last < prev) {
-                        shouldVerify = false;
-                        break;
-                    }
-                }
-
-                if(!shouldVerify)
-                    continue;
-
-                // Calculate sum of powered digits
-                int m = i;
-                int sum = 0;
-                while(m > 0) {
-                    sum += power[m % 10][degree];
-                    m /= 10;
-                }
-
-                if(sum == i && !armstrong.contains(sum)) {
-                    armstrong.add(i);
-                }
-                else {
-                    // Verify if calculated sum is armstrong number
-                    int checksum = 0;
-                    int checkdegree = 0;
-                    int k = sum;
-                    while(k > 0) {
-                        checkdegree++;
-                        k /= 10;
-                    }
-
-                    int l = sum;
-                    while(l > 0) {
-                        checksum += power[l % 10][checkdegree];
-                        l /= 10;
-                    }
-
-                    if(sum == checksum && !armstrong.contains(sum)) {
-                        armstrong.add(sum);
-                    }
-                }
-            }
-            else {
-                armstrong.add(i);
-            }
-        }
-
-        // Convert List with armstrong numbers to array
-        long[] result = new long[armstrong.size()];
-        for(int i = 0; i < armstrong.size(); i++) {
-            result[i] = armstrong.get(i);
-        }
-        Arrays.sort(result);
-        return result;
+        System.out.println();
     }
 }
---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/AbstractFactory.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task37.task3702;
+
+/**
+ * Created by vladislavklockov on 09.07.17.
+ */
+public interface AbstractFactory {
+    Human getPerson(int age);
+}
